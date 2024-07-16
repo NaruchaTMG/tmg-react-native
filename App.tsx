adaccf4d9d8a45422027d3b536c1835bd9e8a557
NaruchaTMG <140366721+NaruchaTMG@users.noreply.github.com>
2024-07-16 14:54:06 +0700

    ScrollView

--- App.tsx
+++ App.tsx
@@ -11,58 +11,78 @@ import {
   Image,
   ImageBackground,
   Platform, Pressable,
-  SafeAreaView,
+  SafeAreaView, ScrollView,
   Text, TextInput, TouchableOpacity,
   View,
 } from 'react-native';
 
+function FirstWidget(props: {
+  onPress: () => void,
+  onPress1: () => void,
+  onChangeText: (text: string) => void
+}) {
+  return <View
+      style={{flex: 1, flexDirection: 'column', backgroundColor: 'green'}}>
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'red',
+    }}>
+      <TouchableOpacity
+          onPress={props.onPress}>
+        <Text
+            style={{fontSize: 20, fontWeight: 'bold'}}>
+          Hello TouchableOpacity!
+        </Text>
+      </TouchableOpacity>
+      <Pressable
+          onPress={props.onPress1}
+      >
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>
+          Hello Pressable!
+        </Text>
+      </Pressable>
+      <TextInput
+          onChangeText={props.onChangeText}
+          style={{height: 45, width: 200, backgroundColor: 'grey'}}/>
+    </View>
+    <View style={{flex: 1, backgroundColor: 'blue', flexDirection: 'row'}}>
+      <Image
+          style={{width: '100%', height: '100%', resizeMode: 'contain'}}
+          source={require('./src/assets/images/bear.jpeg')}/>
+      <Image
+          style={{width: '100%', height: '100%', resizeMode: 'contain'}}
+          source={{uri: 'https://profile.line-scdn.net/0hylJGQ8BXJkN3PjWeQyhZFEt7KC4AECALD11pdQA_fCZeCDFGGQs8JwBsKHVdCmgdHgo8IwY9cHcP'}}/>
+    </View>
+    <View style={{flex: 1, backgroundColor: 'yellow'}}>
+      <ImageBackground
+          style={{flex: 1, padding: 100}}
+          source={require('./src/assets/images/bear.jpeg')}
+      >
+        <View
+            style={{width: 40, height: 40, backgroundColor: 'red'}}></View>
+      </ImageBackground>
+    </View>
+  </View>;
+}
+
 function App(): React.JSX.Element {
   return (
-      <View
-          style={{flex: 1, flexDirection: 'column', backgroundColor: 'green'}}>
-        <View style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'red',
-        }}>
-          <TouchableOpacity
-              onPress={() => Alert.alert('TouchableOpacity')}>
-            <Text
-                style={{fontSize: 20, fontWeight: 'bold'}}>
-              Hello TouchableOpacity!
-            </Text>
-          </TouchableOpacity>
-          <Pressable
-              onPress={() => Alert.alert('PressableOpacity')}
-          >
-            <Text style={{fontSize: 20, fontWeight: 'bold'}}>
-              Hello Pressable!
-            </Text>
-          </Pressable>
-          <TextInput
-              onChangeText={(text: string) => console.log(text)}
-              style={{height: 45, width: 200, backgroundColor: 'grey'}}/>
-        </View>
-        <View style={{flex: 1, backgroundColor: 'blue', flexDirection: 'row'}}>
-          <Image
-              style={{width: '50%', height: '100%', resizeMode: 'contain'}}
-              source={require('./src/assets/images/bear.jpeg')}/>
-          <Image
-              style={{width: '50%', height: '100%', resizeMode: 'contain'}}
-              source={{uri: 'https://profile.line-scdn.net/0hylJGQ8BXJkN3PjWeQyhZFEt7KC4AECALD11pdQA_fCZeCDFGGQs8JwBsKHVdCmgdHgo8IwY9cHcP'}}/>
-        </View>
-        <View style={{flex: 1, backgroundColor: 'yellow'}}>
-          <ImageBackground
-              style={{flex: 1, padding: 50}}
-              source={require('./src/assets/images/bear.jpeg')}
-          >
-            <View
-                style={{width: 40, height: 40, backgroundColor: 'red'}}></View>
-          </ImageBackground>
-        </View>
-      </View>
-
+     <SafeAreaView style={{flex: 1}}>
+       <ScrollView
+           horizontal={false}
+           style={{flex: 1, backgroundColor: "white"}}
+           contentContainerStyle={{backgroundColor: "blue"}}
+           showsVerticalScrollIndicator={false}
+       >
+         <View style={{width: "100%", height: 350, backgroundColor: "yellow"}} />
+         <View style={{width: "100%", height: 350, backgroundColor: "green"}} />
+         <View style={{width: "100%", height: 350,  backgroundColor: "blue"}} />
+         <View style={{width: "100%", height: 350,  backgroundColor: "purple"}} />
+         <View style={{width: "100%", height: 350,  backgroundColor: "grey"}} />
+       </ScrollView>
+     </SafeAreaView>
   );
 }
 
