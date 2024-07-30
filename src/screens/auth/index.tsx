27b4e67cf391faf572ac9dab0bc00ddbdff132fd
NaruchaTMG <140366721+NaruchaTMG@users.noreply.github.com>
2024-07-30 11:39:22 +0700

    replace checkbox

--- src/screens/auth/index.tsx
+++ src/screens/auth/index.tsx
@@ -7,42 +7,98 @@ import {
 } from 'react-native';
 import {styles} from './styles.ts';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {CustomTextInput} from '../../components/text/CustomTextInput.tsx';
+import {CustomTextInput} from '../../components/text/input';
+import MainButton from '../../components/buttons/main';
+import CustomCheckBox from '../../components/buttons/checkbox';
+import {useState} from 'react';
+import {p1, p2, p3} from '../../Constants.ts';
+
+type ConditionProps = {
+  p1: boolean;
+  p2: boolean;
+  p3: boolean;
+}
 
 function AuthScreen() {
-  const insets = useSafeAreaInsets()
+  const [conditions, setConditions] = useState<ConditionProps | undefined>(
+      undefined);
+  const insets = useSafeAreaInsets();
+
+  function onPressCondition(tag: string, val: boolean) {
+    setConditions(prevState =>
+        prevState ? ({
+          ...prevState,
+          [tag]: !val,
+        }) : ({
+          p1: tag === "p1",
+          p2: tag === "p2",
+          p3: tag === "p3",
+        }));
+  }
+
   return (
-      <ScrollView style={[styles.container]}>
+      <ScrollView
+          style={[styles.container]}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{paddingBottom: insets.bottom}}>
         <ImageBackground
             style={[styles.bgContainer, {paddingTop: insets.top}]}
             source={require('../../assets/images/bg-login.jpg')}
-            resizeMode={"cover"}
+            resizeMode={'contain'}
         >
-          <View style={[styles.container, {alignItems: 'flex-end', paddingHorizontal: 20}]}>
+          <View style={[
+            styles.container,
+            {alignItems: 'flex-end', paddingHorizontal: 20}]}>
             <TouchableOpacity>
-              <Text style={[styles.notosanSemiBold, {fontSize: 16, color: "white"}]}>Skip</Text>
+              <Text style={[
+                styles.notosanSemiBold,
+                {fontSize: 16, color: 'white'}]}>Skip</Text>
             </TouchableOpacity>
           </View>
-          <Text style={[styles.notosanSemiBold, {fontSize: 24, color: "white", alignItems: "flex-end", marginLeft: 20}]}>
+          <Text style={[
+            styles.notosanSemiBold,
             {
-              "ยินดีต้อนรับ\nเข้าสู่ระบบสมาชิก M Card"
+              fontSize: 24,
+              color: 'white',
+              alignItems: 'flex-end',
+              padding: 20,
+            }]}>
+            {
+              'ยินดีต้อนรับ\nเข้าสู่ระบบสมาชิก M Card'
             }
           </Text>
         </ImageBackground>
         <View style={styles.formContainer}>
-          <CustomTextInput placeholder={"Id card number*"} />
-          <CustomTextInput placeholder={"Telephone number*"} />
-          <CustomTextInput placeholder={"Refer code*"} />
+          <CustomTextInput placeholder={'Id card number*'}/>
+          <CustomTextInput placeholder={'Telephone number*'}/>
+          <CustomTextInput placeholder={'Refer code*'}/>
         </View>
-        <View style={[styles.container, {paddingHorizontal: 20}]}>
-          <Pressable style={styles.buttonContainer}>
-            <Text style={[styles.notosanBold, {color: "white", fontSize: 16}]}>
-              {"Submit"}
-            </Text>
-          </Pressable>
+        <View style={styles.checkboxContainer}>
+          <CustomCheckBox tag={'p1'}
+                          isChecked={conditions?.p1 || false}
+                          text={p1}
+                          onPress={(tag: string, val) => {
+                            onPressCondition(tag, val);
+                          }}/>
+          <CustomCheckBox tag={'p2'}
+                          isChecked={conditions?.p2 || false}
+                          text={p2}
+                          onPress={(tag: string, val) => {
+                            onPressCondition(tag, val);
+                          }}/>
+          <CustomCheckBox tag={'p3'}
+                          isChecked={conditions?.p3 || false}
+                          text={p3}
+                          onPress={(tag: string, val) => {
+                            onPressCondition(tag, val);
+                          }}/>
+        </View>
+        <View
+            style={[styles.container, {paddingHorizontal: 20, paddingTop: 20}]}>
+          <MainButton />
         </View>
       </ScrollView>
-  )
+  );
 }
 
-export default AuthScreen
+export default AuthScreen;
--- src/screens/auth/styles.ts
+++ src/screens/auth/styles.ts
@@ -1,47 +1,44 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   bgContainer: {
-    width: "100%",
+    width: '100%',
     height: undefined,
-    aspectRatio: 1,
-    justifyContent: "space-between"
+    aspectRatio: 2660 / 2261,
+    justifyContent: 'space-between',
   },
   formContainer: {
-    width: "100%",
+    width: '100%',
     height: undefined,
     padding: 20,
-    gap: 20
+    gap: 20,
   },
   textInput: {
-    width: "100%",
+    width: '100%',
     height: 45,
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: "grey",
-    padding: 10
+    borderColor: 'grey',
+    padding: 10,
+  },
+  checkboxContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    gap: 10
   },
   notosanRegular: {
-    fontFamily: "NotoSansThai-Regular"
+    fontFamily: 'NotoSansThai-Regular',
   },
   notosanMedium: {
-    fontFamily: "NotoSansThai-Medium"
+    fontFamily: 'NotoSansThai-Medium',
   },
   notosanSemiBold: {
-    fontFamily: "NotoSansThai-SemiBold"
+    fontFamily: 'NotoSansThai-SemiBold',
   },
   notosanBold: {
-    fontFamily: "NotoSansThai-Bold"
+    fontFamily: 'NotoSansThai-Bold',
   },
-  buttonContainer: {
-    width: "100%",
-    backgroundColor: 'red',
-    height: 50,
-    borderRadius: 25,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-})
+});
