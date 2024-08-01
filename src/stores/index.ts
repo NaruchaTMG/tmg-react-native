cb794bfcda8f310f4b95e67662b0bb6fbabbed19
NaruchaTMG <140366721+NaruchaTMG@users.noreply.github.com>
2024-08-01 16:56:03 +0700

    Initial store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,11 +1,33 @@
 import {createContext, useContext} from 'react';
-import {makeAutoObservable} from 'mobx';
+import {action, computed, makeObservable, observable} from 'mobx';
 
 export class Store {
   identity: string = "720011100022233"
+  count: number = 0
+  counter = 0
 
   constructor() {
-    makeAutoObservable(this)
+    makeObservable(this, {
+      count: observable,
+      counter: observable,
+      doubleCount: computed,
+      increment: action,
+      decrement: action,
+    })
+  }
+
+  get doubleCount() {
+    return this.count * 2;
+  }
+
+  increment() {
+    this.count += 1
+    console.log(this.count)
+  }
+
+  decrement() {
+    this.count -= 1
+    console.log(this.count)
   }
 }
 
